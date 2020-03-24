26d38e01bd66f79beedceeb9b96d2758f033e7be
Devin Dias <devindias.tech@gmail.com>
2020-03-24 08:29:59 -0500

    registers , installs , and activates service worker

---
+++ sw_cached.js
@@ -0,0 +1,10 @@
+
+// // Call Install Event
+self.addEventListener('install', e => {
+    console.log('Service Worker: Installed');
+  });
+  
+//   // Call Activate Event
+//   self.addEventListener('activate', e => {
+//     console.log('Service Worker: Activated')
+//   })
