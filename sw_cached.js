ba2afd79ceeca58f9b93192f6afc2adbe73953dc
Devin Dias <devindias.tech@gmail.com>
2020-03-24 15:18:52 -0500

    completed site service wokrer

--- sw_cached.js
+++ sw_cached.js
@@ -1,10 +1,41 @@
+const cacheName = "cacheName";
 
-// // Call Install Event
-self.addEventListener('install', e => {
-    console.log('Service Worker: Installed');
-  });
-  
-//   // Call Activate Event
-//   self.addEventListener('activate', e => {
-//     console.log('Service Worker: Activated')
-//   })
+const cacheFiles = ["index.html", "about.html", "index.css", "main.js"];
+// Call Install Event
+self.addEventListener("install", e => {
+  console.log("Service Worker: Installed");
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache => {
+        console.log("service worker caching");
+        cache.addAll(cacheFiles);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Call Activate Event
+self.addEventListener("activate", e => {
+  console.log("Service Worker: Activated");
+  //remove old cache
+  e.waitUntil(
+    caches.keys().then(names => {
+      return Promise.all(
+        names.map(x => {
+          if (x !== cacheName) {
+            console.log("SW clearing old cache");
+            return caches.delete(x);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  console.log("Service Worker: Fetching");
+  e.respondWith(
+    fetch(e.request).catch(err => caches.match(e.request).then(res => res))
+  );
+});
